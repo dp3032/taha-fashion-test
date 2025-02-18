b3560b454f02165a74d521858bb3b036caaf393c
dp3032 <use32300@gmail.com>
2025-02-18 11:40:16 +0530

    Update Paymentfail.js

--- frontend/src/payment/Paymentfail.js
+++ frontend/src/payment/Paymentfail.js
@@ -12,23 +12,23 @@ function PaymentFailure() {
   return (
     <>
       <Header />
-      <div className="failure-container">
-        <div className="failure-message">
-          <div className="failure-character">
+        <div className="thank-you-container">
+          <div className="thank-you-message">
+            <div className="character-animation">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="100" height="100">
-              <circle cx="50" cy="50" r="40" fill="lightgray" className="failure-circle"/>
-              <circle cx="40" cy="40" r="5" fill="black"/>
-              <circle cx="60" cy="40" r="5" fill="black"/>
-              <path d="M35,60 Q50,50 65,60" stroke="black" strokeWidth="2" fill="transparent"/>
-            </svg>
+                <circle cx="50" cy="50" r="40" fill="lightgray" className="failure-circle"/>
+                <circle cx="40" cy="40" r="5" fill="black"/>
+                <circle cx="60" cy="40" r="5" fill="black"/>
+                <path d="M35,60 Q50,50 65,60" stroke="black" strokeWidth="2" fill="transparent"/>
+              </svg>
+            </div>
+            <p>Payment Failed...!</p>
+            <p>Your order has been Failed...!</p>
+            <button className=" btn btn-error animated-button " onClick={navigateHome}>
+              Go to Homepage
+            </button>
           </div>
-          <p>Payment Failed...!</p>
-          <p>Your order has been Failed...!</p>
-          <button className="btn-error" onClick={navigateHome}>
-            Go to Homepage
-          </button>
         </div>
-      </div>
       <Footer />
     </>
   );
